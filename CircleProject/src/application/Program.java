e035234512656a218802702c4ce4d0ba98fda13a
linco <linco@DESKTOP-VIJS1C2>
2022-04-13 12:53:25 -0300

    Second commit - Object Calculator

--- CircleProject/src/application/Program.java
+++ CircleProject/src/application/Program.java
@@ -3,9 +3,10 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import util.Calculator;
+
 public class Program {
-	
-	public static final double PI = 3.14159;
+
 
 	public static void main(String[] args) {
 
@@ -14,25 +15,18 @@ public class Program {
 		
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
+		Calculator calc = new Calculator();
 		
-		double c = circumference(radius);
+		double c = calc.circumference(radius);
 		
-		double v = volume(radius);
+		double v = calc.volume(radius);
 		
 		System.out.printf("Circunferemce: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", PI);
+		System.out.printf("PI value: %.2f%n", calc.PI);
 
 		sc.close();
 		
-	}
-	
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-	
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius / 3.0;
 	}
 
 }
